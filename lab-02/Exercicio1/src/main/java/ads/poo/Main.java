675c042ec27495a906eaede441516437679040c1
arthurabdala-ifsc <arthur.ma2005@aluno.ifsc.edu.br>
2026-02-26 20:25:56 -0300

    Criando repositório do lab-02 e iniciando o exercicio 1

---
+++ lab-02/Exercicio1/src/main/java/ads/poo/Main.java
@@ -0,0 +1,13 @@
+package ads.poo;
+
+public class Main {
+    static void main() {
+
+        String dia = IO.readln("Entre com o dia: ");
+        int d = Integer.parseInt(dia);
+
+        String mes = IO.readln("Entre com o mês: ");
+
+
+    }
+}

1b6b565dd3f72a262d5f3f970e5040364376e83d
arthurabdala-ifsc <arthur.ma2005@aluno.ifsc.edu.br>
2026-04-09 20:19:49 -0300

    Criando algoritmo para gestão de acervo de uma Livraria

---
+++ aula-15/src/main/java/ads/poo/Livro.java
@@ -0,0 +1,18 @@
+package ads.poo;
+
+import java.util.HashMap;
+
+public class Livro {
+
+    private String titulo;
+    private String nomeAutor;
+    private int ano;
+    private String isbn;
+
+    public Livro(String titulo, String nomeAutor, int ano, String isbn) {
+        this.titulo = titulo;
+        this.nomeAutor = nomeAutor;
+        this.ano = ano;
+        this.isbn = isbn;
+    }
+}

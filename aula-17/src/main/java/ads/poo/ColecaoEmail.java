354835bfa6a916ee7efd495ddeaf86d0cf19e54e
arthurabdala-ifsc <arthur.ma2005@aluno.ifsc.edu.br>
2026-04-28 20:08:45 -0300

    Criando base das classes ColecaoTelefone e ColecaoEmail

---
+++ aula-17/src/main/java/ads/poo/ColecaoEmail.java
@@ -0,0 +1,34 @@
+package ads.poo;
+
+import java.util.HashMap;
+
+public class ColecaoEmail {
+
+    private HashMap<String, String> dados;
+
+    public  boolean add(String rotulo, String valor){
+        if (true) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean remove(String rotulo, String valor){
+        if (true){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean update(String rotulo, String valor){
+        if (true){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString(){
+        return "EmailFormatado";
+    }
+}
